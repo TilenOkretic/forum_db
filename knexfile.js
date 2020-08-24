4f9ef24d9dbcaccc47bb419772a598d2402c48a8
TilenOkretic <tilen.okretic@gmail.com>
2020-08-24 22:00:21 +0200

    second

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://Shadow:admin@localhost:5432/myforum'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
 
